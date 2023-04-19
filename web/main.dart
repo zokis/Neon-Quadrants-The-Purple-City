93e0c8390b26939a739ffe4a46650b6bb893a1fa
Marcelo Fonseca Tambalo <marcelo.tambalo@nectosystems.com.br>
2023-04-19 01:20:37 -0300

    more lots

--- web/main.dart
+++ web/main.dart
@@ -9,29 +9,41 @@ import 'player.dart';
 import 'bridge.dart';
 
 
+Game gameUriFactory(){
+  final String? param = Uri.base.queryParameters['rabfle'];
+  if (param != null){
+    final Map<String, dynamic> gameMap = decodeZ(param);
+    final List<dynamic> playersList = gameMap['players'] as List<dynamic>;
+    final List<Player> players = playersList.map((playerData) => Player(playerData['name'] as String)).toList();
+    return Game(players, gameMap["grid"][0] as int, gameMap["grid"][1] as int);
+  }
+  return Game([Player('human'), Player('computer 1')], 8, 13);
+}
+
+
 void main() {
+  final Game g = gameUriFactory();
   final TableElement table = querySelector('#battleground-table') as TableElement;
   final DivElement battleground = querySelector('#battleground') as DivElement;
   final DivElement loading = querySelector('#loading-screen') as DivElement;
-  final Game g = Game([Player('human'), Player('computer 1')], 8, 13);
-  final Element player1Html = querySelector('#player1') as Element;
-  final Element player2Html = querySelector('#player2') as Element;
 
-  player1Html.text = g.players[0].name;
-  player2Html.text = g.players[1].name;
+  g.players.asMap().forEach((int i, Player p){
+    final Element playerHtml = querySelector('#player${i+1}') as Element;
+    playerHtml.text = g.players[i].name;
+  });
 
   // Printing the matrix
-  List<List<Square>> matrix = g.grid.matrix;
-  for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix[i].length; j++) {
-      print('Square at ($i, $j, ${matrix[i][j].completed}):');
-      print('Left: ${matrix[i][j].left}');
-      print('Top: ${matrix[i][j].top}');
-      print('Right: ${matrix[i][j].right}');
-      print('Bottom: ${matrix[i][j].bottom}');
-      print('');
-    }
-  }
+  // List<List<Square>> matrix = g.grid.matrix;
+  // for (int i = 0; i < matrix.length; i++) {
+  //   for (int j = 0; j < matrix[i].length; j++) {
+  //     print('Square at ($i, $j, ${matrix[i][j].completed}):');
+  //     print('Left: ${matrix[i][j].left}');
+  //     print('Top: ${matrix[i][j].top}');
+  //     print('Right: ${matrix[i][j].right}');
+  //     print('Bottom: ${matrix[i][j].bottom}');
+  //     print('');
+  //   }
+  // }
 
   final Bridge bridge = Bridge(table, g);
   bridge.renderGridHTML();
@@ -41,4 +53,19 @@ void main() {
   loading.remove();
   table.animate([{"opacity": 0}, {"opacity": 100}], 800);
   table.style.setProperty("opacity", "100");
+
+
+  print(encodeZ({
+    "grid": [8, 8],
+    "players": [
+      {
+        "name": "Zokis",
+        "d": 0
+      },
+      {
+        "name": "Robot",
+        "d": 1
+      }
+    ]
+  }));
 }
--- web/utils.dart
+++ web/utils.dart
@@ -1,14 +1,30 @@
 import 'dart:math';
+import 'dart:convert';
+
 // Helper method to generate a random string as a unique identifier
 String generateRandomString() {
-  const _chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const String _chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   final _rnd = Random();
   return String.fromCharCodes(Iterable.generate(10, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 }
 
 String getRndBg() {
-  List<int> bgs = [1, 2, 3, 4];
+  final List<int> bgs = [1, 2, 3, 4];
   final _rnd = Random();
-  int bgn = bgs[_rnd.nextInt(bgs.length)];
+  final int bgn = bgs[_rnd.nextInt(bgs.length)];
   return "url('static/bg${bgn}.png')";
+}
+
+Map<String, dynamic> decodeZ(String p){
+	final List<int> decodedBytes = base64.decode(p.split('').reversed.join());
+	final String decodedJson = utf8.decode(decodedBytes);
+	final String jsonWithoutSalt = decodedJson.replaceAll('EMTAd3.1', '');
+	return jsonDecode(jsonWithoutSalt);
+}
+
+String encodeZ(Map<String, dynamic> m){
+	final String jsonStr = jsonEncode(m);
+	final List<int> bytesToEncode = utf8.encode(jsonStr + 'EMTAd3.1');
+	final String base64Str = base64.encode(bytesToEncode);
+	return base64Str.split('').reversed.join();
 }
